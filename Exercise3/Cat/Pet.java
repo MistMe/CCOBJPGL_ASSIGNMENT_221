fcee717d20f53fc26d32cfdf046fcf737a58dae7
Lyod Rhussel Torrenueva <111472869+MistMe@users.noreply.github.com>
2023-05-11 09:25:49 +0800

    Add files via upload

---
+++ Exercise3/Cat/Pet.java
@@ -0,0 +1,7 @@
+public class Pet implements Bombay {
+
+    @Override
+    public void meow() {
+        System.out.println("My cute Bombay cat is meowing");
+    };
+}
---
+++ Exercise3/Cat/Shorthair.java
@@ -0,0 +1,7 @@
+interface Shorthair {
+
+        String fur = "short";
+    
+        void meow();
+    }
+
